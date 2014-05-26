c2f614cff9640c947920cc00172adcb31fdc1961
Lautaro Orazi <orazile@gmail.com>
2014-05-26 20:06:46 -0300

    Add job: add_result

--- jobs/add_result.rb
+++ jobs/add_result.rb
@@ -1,11 +1,26 @@
-require_relative "../app"
+require "./app"
 require 'eventmachine'
+require 'tzinfo'
 require 'logger'
 
 LOGGER= Logger.new('jobs/logs/add_result.log')
 
 module ResultsJob
   def self.run
+    Match.all.each do |match|
+      next unless match.result.nil?
+      tz = TZInfo::Timezone.get("Etc/#{match.local_timezone}")
+      #In order to conform with the POSIX style, those zones beginning with "Etc/GMT" have their sign reversed from what most people expect. In this style, zones west of GMT have a positive sign and those east have a negative sign.
+      local = tz.local_to_utc(Time.now.utc)
+
+      if local >= match.start_datetime
+        begin
+          Result.create(match_id: match.id)
+        rescue => e
+          LOGGER.error e.message + " " + e.backtrace
+        end
+      end
+    end
   end
 end
 
