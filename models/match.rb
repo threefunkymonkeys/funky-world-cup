7745e3aa602dc848070c4a63487df8fed0c86146
Lautaro Orazi <orazile@gmail.com>
2014-07-09 21:50:16 -0300

    Fix variables

--- models/match.rb
+++ models/match.rb
@@ -10,7 +10,7 @@ class Match < Sequel::Model
   def update_predictions_score
     match_predictions.each do |prediction|
       prediction.update_score(result)
-      if (result.host_score == result.rival_score) && (prediction.host_score == predition.rival_score) && result.match.allow_penalties? #ignore if cheating
+      if (result.host_score == result.rival_score) && (prediction.host_score == prediction.rival_score) && result.match.allow_penalties? #ignore if cheating
         prediction.match_penalties_prediction.update_score(result) unless prediction.match_penalties_prediction.nil?
       end
     end
