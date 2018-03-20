574e5c2089b6f56e4f5e6061a2fdea9e78a54f3b
Leonardo Mateo <leonardomateo@gmail.com>
2018-03-19 22:38:30 -0300

    Add example dashboard template and assets files

--- routes/cup_groups.rb
+++ routes/cup_groups.rb
@@ -10,7 +10,7 @@ module FunkyWorldCup
       end
 
       on get, "fixture" do
-        res.write render("./views/layouts/application.html.erb") {
+        res.write render("./views/layouts/application_2014.html.erb") {
           render("./views/cup_groups/fixture.html.erb", fixture: CupGroup.join(:matches, group_id: :id).order(:start_datetime).to_hash_groups(:phase) )
         }
       end
