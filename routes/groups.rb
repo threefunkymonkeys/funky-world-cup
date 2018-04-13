ff09b23a1595d77d7c4e6063757d8eaade7bfc8c
Lautaro Orazi <orazile@gmail.com>
2018-04-13 09:30:09 -0300

    Create Groups dashboard.

--- routes/groups.rb
+++ routes/groups.rb
@@ -102,53 +102,52 @@ module FunkyWorldCup
           end
 
           on ":id" do |group_id|
-            on (group = Group[group_id.to_i]) do
-              on group.user_id == current_user.id do
-                on root do
-                  attrs = req.params['group'].strip
-                  if FunkyWorldCup::GroupUpdate.new(self, group).execute(attrs)
-                    res.redirect "/groups/#{group.id}"
-                  else
-                    res.redirect "/groups/#{group.id}/edit"
-                  end
-                end
+            group = Group[group_id.to_i]
+            not_found! unless group
 
-                on "prizes" do
-                  old = group.group_prizes
-
-                  new = Array.new
-                  begin
-                    FunkyWorldCup::Helpers.database.transaction(rollback: :reraise) do
-                      req.params['prizes'].each_with_index do |prize, index|
-                        GroupPrize.create(name: prize, group_id: group.id, order: index +1)
-                      end
-
-                      old.each do |prize|
-                        prize.delete
-                      end
-                    end
-                    flash[:success] = I18n.t('.messages.prizes.list_updated')
-                    res.redirect "/groups/#{group.id}"
-                  rescue Sequel::Rollback
-                    flash[:error] = "#{I18n.t('.messages.prizes.cant_save_list')}, #{I18n.t('.messages.common.please')} #{I18n.t('.messages.common.try_again')}"
-                    session['fwc.pizes'] = req.params['prizes']
-                    res.redirect "/groups/#{group.id}/prizes"
-                  end
-                end
-
-                on "reset_link" do
-                  begin
-                    group.link = FunkyWorldCupApp::generate_group_link(group.id)
-                    group.save
-                    flash[:success] = I18n.t('.messages.groups.link_updated')
-                  rescue => e
-                    puts e.inspect
-                    flash[:error] = "#{I18n.t('.messages.groups.cant_update_link')}, #{I18n.t('.messages.common.please')} #{I18n.t('.messages.common.try_again')}"
-                  end
+            on group.user_id == current_user.id do
+              on root do
+                attrs = req.params['group'].strip
+                if FunkyWorldCup::GroupUpdate.new(self, group).execute(attrs)
                   res.redirect "/groups/#{group.id}"
+                else
+                  res.redirect "/groups/#{group.id}/edit"
                 end
+              end
 
-                not_found!
+              on "prizes" do
+                old = group.group_prizes
+
+                new = Array.new
+                begin
+                  FunkyWorldCup::Helpers.database.transaction(rollback: :reraise) do
+                    req.params['prizes'].each_with_index do |prize, index|
+                      GroupPrize.create(name: prize, group_id: group.id, order: index +1)
+                    end
+
+                    old.each do |prize|
+                      prize.delete
+                    end
+                  end
+                  flash[:success] = I18n.t('.messages.prizes.list_updated')
+                  res.redirect "/groups/#{group.id}"
+                rescue Sequel::Rollback
+                  flash[:error] = "#{I18n.t('.messages.prizes.cant_save_list')}, #{I18n.t('.messages.common.please')} #{I18n.t('.messages.common.try_again')}"
+                  session['fwc.pizes'] = req.params['prizes']
+                  res.redirect "/groups/#{group.id}/prizes"
+                end
+              end
+
+              on "reset_link" do
+                begin
+                  group.link = FunkyWorldCupApp::generate_group_link(group.id)
+                  group.save
+                  flash[:success] = I18n.t('.messages.groups.link_updated')
+                rescue => e
+                  puts e.inspect
+                  flash[:error] = "#{I18n.t('.messages.groups.cant_update_link')}, #{I18n.t('.messages.common.please')} #{I18n.t('.messages.common.try_again')}"
+                end
+                res.redirect "/groups/#{group.id}"
               end
 
               not_found!
